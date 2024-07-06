69ea08c01b05dd7823d48601ca18ddcee992d122
Muhammad Karimov <shermuhammad.karimov2004@mail.ru>
2024-07-06 08:49:25 +0500

    final mvc conroller learn

---
+++ src/test/java/com/learnMVC/learnMVC/LearnMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnMVC.learnMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
